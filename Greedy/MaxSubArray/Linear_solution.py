4e94fc5be0ec495c9d494292afb5f72cf45bb04d
Indroneel Roy <indroneel1000@gmail.com>
2025-04-07 18:08:05 +0600

    Linear maxSubArray

---
+++ Greedy/MaxSubArray/Linear_solution.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = nums[0]
+        curr = 0
+        for i in range(len(nums)):
+            if curr < 0 :
+                curr = 0
+            curr += nums[i]
+            res = max(res, curr)
+        return res
+    
+if __name__ == "__main__":
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    s = Solution()
+    print(s.maxSubArray(nums))                    
