c17813ed505f94b5f1ce8c745f74f0040a9ca90d
Indroneel Roy <indroneel1000@gmail.com>
2025-04-08 14:49:44 +0600

    add DP method for Jump Game

---
+++ Greedy/Jump Game/dp_method.py
@@ -0,0 +1,25 @@
+from typing import List
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        memo = {}
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i == len(nums) - 1:
+                return True
+            if nums[i] == 0:
+                return False
+            
+            end = min(len(nums), i + nums[i] + 1)
+            for j in range(i+1, end):
+                if dfs(j):
+                    memo[i] = True
+                    return True
+            memo[i] = False
+            return False
+        return dfs(0)    
+
+if __name__ == "__main__":
+    nums = [2,3,1,1,4]
+    s = Solution()
+    print(s.canJump(nums))                
