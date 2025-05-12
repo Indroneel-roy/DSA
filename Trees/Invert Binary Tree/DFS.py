5ccae30cd7d6793c616759235e87df581ad53bae
Indroneel Roy <indroneel1000@gmail.com>
2025-05-12 19:10:02 +0600

    Invert binary tree

---
+++ Trees/Invert Binary Tree/DFS.py
@@ -0,0 +1,22 @@
+# Problem Link : https://leetcode.com/problems/invert-binary-tree/description/
+from typing import List, Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        #swap the tree
+        temp = root.left
+        root.left = root.right
+        root.right = temp   
+        
+        #DFS iteration
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root 
