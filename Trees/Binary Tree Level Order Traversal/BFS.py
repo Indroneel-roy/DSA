5b0355775fabd5907d322646798c81a9a48d6b04
Indroneel Roy <indroneel1000@gmail.com>
2025-05-21 11:12:04 +0600

    BFS

---
+++ Trees/Binary Tree Level Order Traversal/BFS.py
@@ -0,0 +1,60 @@
+# Promblem Link : https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+from typing import List, Optional
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = deque()
+        q.append(root)
+        while q:
+            qlen = len(q)
+            level = []
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:        
+                res.append(level)
+        return res   
+
+##Helper function to create tree
+def build_tree(nodes: List[Optional[int]]) -> Optional[TreeNode]:
+    if not nodes:
+        return None
+
+    root = TreeNode(nodes[0])
+    queue = deque([root])
+    index = 1
+
+    while queue and index < len(nodes):
+        node = queue.popleft()
+
+        # Left child
+        if index < len(nodes) and nodes[index] is not None:
+            node.left = TreeNode(nodes[index])
+            queue.append(node.left)
+        index += 1
+
+        # Right child
+        if index < len(nodes) and nodes[index] is not None:
+            node.right = TreeNode(nodes[index])
+            queue.append(node.right)
+        index += 1
+
+    return root     
+if __name__ == "__main__":
+    root_val = [3,9,20,None,None,15,7]
+    root = build_tree(root_val)
+    sol = Solution()
+    res = sol.levelOrder(root)
+    print(res)             
+                
