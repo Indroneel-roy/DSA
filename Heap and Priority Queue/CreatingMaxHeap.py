1aba8b6178942e5915d8b4aedf241192dcb9b4e8
Indroneel Roy <indroneel1000@gmail.com>
2025-06-11 23:21:10 +0600

    Heap

---
+++ Heap and Priority Queue/CreatingMaxHeap.py
@@ -0,0 +1,22 @@
+# Array = [19, 7, 17, 3, 5, 12, 10, 1, 2] this maxheap left child of root will stay in 2*n nth possition and right child 2*n + 1 and index will start from 1
+heap = [None] * 10
+heap[1] = 19
+heap[1*2] = 7
+heap[1*2+1] = 17
+heap[2*2] = 3
+heap[2*2+1] = 5
+heap[3*2] = 12
+heap[3*2+1] = 10
+heap[4*2] = 1
+heap[4*2+1] = 2
+print(heap)
+
+#find left and right child and parent node
+def left(i):
+    return 2 * i
+def right(i):
+    return 2 * i + 1
+def parent(i):
+    return i // 2
+print(heap[left(3)])
+
