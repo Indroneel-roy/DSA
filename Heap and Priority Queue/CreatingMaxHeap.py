2107c46bec14684f8330ed06c376d07e1b8cba05
Indroneel Roy <indroneel1000@gmail.com>
2025-06-15 23:52:15 +0600

    Theory

--- Heap and Priority Queue/CreatingMaxHeap.py
+++ Heap and Priority Queue/CreatingMaxHeap.py
@@ -20,3 +20,18 @@ def parent(i):
     return i // 2
 print(heap[left(3)])
 
+#Justyfy MaxHeap or not
+
+def is_max_heap(H):
+    n = len(H) - 1
+    for i in range(n, 1, -1):
+        p = parent(i)
+        if H[p] < H[i]:
+            return False
+    return True
+if __name__ == "__main__":
+    H = [None, 19, 7, 17, 3, 5, 12, 10, 1, 2] 
+    print(H, is_max_heap(H))
+    
+        
+
