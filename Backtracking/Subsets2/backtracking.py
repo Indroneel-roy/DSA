ceec751a2c51138111776d968c49b82862edcc2c
Indroneel Roy <indroneel1000@gmail.com>
2025-08-10 13:43:46 +0600

    subset2

---
+++ Backtracking/Subsets2/backtracking.py
@@ -0,0 +1,25 @@
+# Problem Link : https://leetcode.com/problems/subsets-ii/description/
+from typing import List
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset)
+        backtrack(0, [])
+        return res    
+if __name__ == "__main__":
+    nums = [1,2,2] 
+    sol = Solution()
+    print(sol.subsetsWithDup(nums))           
+     
+        
