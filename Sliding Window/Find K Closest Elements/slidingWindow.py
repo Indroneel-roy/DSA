06663d5bf6eda10ac0e607dae47a9049047ece8e
Indroneel Roy <indroneel1000@gmail.com>
2025-09-17 21:25:25 +0600

    slidingWindow

---
+++ Sliding Window/Find K Closest Elements/slidingWindow.py
@@ -0,0 +1,17 @@
+# Problem Link : https://leetcode.com/problems/find-k-closest-elements/
+from typing import List
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - k
+        while l < r:
+            m = (l + r) // 2
+            if x - arr[m] > arr[m + k] - x:
+                l = m + 1
+            else:
+                r = m
+        return arr[l : l + k]            
+if __name__ == "__main__":
+    arr = [1,2,3,4,5]
+    k = 4
+    x = 3
+    print(Solution().findClosestElements(arr, k, x))    
