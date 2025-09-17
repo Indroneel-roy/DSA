0de9eb4858ef19ab32daa75a17d58b2c81519988
Indroneel Roy <indroneel1000@gmail.com>
2025-09-17 21:52:49 +0600

    stack

---
+++ Stack/Baseball Game/stack.py
@@ -0,0 +1,19 @@
+# Problem Link : https://leetcode.com/problems/find-k-closest-elements/
+from typing import List
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for op in operations:
+            if op == "+":
+                stack.append(stack[-1] + stack[-2])
+            elif op == "C":
+                stack.pop()
+            elif op == "D":
+                stack.append(2 * stack[-1])
+            else:
+                stack.append(int(op))
+        return sum(stack)                    
+    
+if __name__ == "__main__":
+    operations = ["5","2","C","D","+"]
+    print(Solution().calPoints(operations))    
