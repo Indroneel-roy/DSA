b375a252f335f6838f30b43a3414671bc0dc33ed
Indroneel Roy <indroneel1000@gmail.com>
2026-01-06 22:25:51 +0600

    stock price

---
+++ Hashing and Arrays/Best Time to Buy and Sell Stock/max_profit.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float("inf")
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:    
+                max_profit = max(max_profit, price - min_price)  
+        return max_profit   
+if __name__ == "__main__":
+    sol = Solution()
+    prices = [7,1,5,3,6,4]
+    print(sol.maxProfit(prices))
+        
